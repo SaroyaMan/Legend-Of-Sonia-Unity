5b655a98ca65198e4fac5b6d7c621722ff059842
Yoav Saroya <stankovic100@gmail.com>
2017-08-09 02:49:58 +0300

    Updating SFX to PowerUps

--- Assets/Scripts/HealthPowerUp.cs
+++ Assets/Scripts/HealthPowerUp.cs
@@ -6,18 +6,24 @@ public class HealthPowerUp : MonoBehaviour {
 
     private GameObject player;
     private PlayerHealth playerHealth;
+    private AudioSource audioSource;
+    private SpriteRenderer sprite;
 
-	private void Start () {
+    private void Start () {
         player = GameManager.instance.Player;
         playerHealth = player.GetComponent<PlayerHealth>();
+        audioSource = GetComponent<AudioSource>();
+        sprite = GetComponent<SpriteRenderer>();
         GameManager.instance.RegisterPowerUp();
     }
 
     private void OnTriggerEnter(Collider other) {
         if(other.gameObject == player) {
+            sprite.enabled = false;
+            audioSource.PlayOneShot(audioSource.clip);
             playerHealth.PowerUpHealth(healthToAdd);
             GameManager.instance.UnregisterPowerUp();
-            Destroy(gameObject);
+            Destroy(gameObject, audioSource.clip.length);
         }
     }
 }
--- Assets/Scripts/SpeedPowerUp.cs
+++ Assets/Scripts/SpeedPowerUp.cs
@@ -4,18 +4,24 @@ public class SpeedPowerUp: MonoBehaviour {
 
     private GameObject player;
     private PlayerController playerController;
+    private AudioSource audioSource;
+    private SpriteRenderer sprite;
 
     private void Start() {
         player = GameManager.instance.Player;
         playerController = player.GetComponent<PlayerController>();
+        audioSource = GetComponent<AudioSource>();
+        sprite = GetComponent<SpriteRenderer>();
         GameManager.instance.RegisterPowerUp();
     }
 
     private void OnTriggerEnter(Collider other) {
         if(other.gameObject == player) {
+            sprite.enabled = false;
+            audioSource.PlayOneShot(audioSource.clip);
             playerController.PowerUpSpeed();
             GameManager.instance.UnregisterPowerUp();
-            Destroy(gameObject);
+            Destroy(gameObject, audioSource.clip.length);
         }
     }
 }
